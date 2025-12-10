e2a9bd23d1a9e9a08db73f6781bf541bd48c9908
amcaughan <77643664+amcaughan@users.noreply.github.com>
2025-12-09 18:05:42 -0800

    Added terragrunt stuff

--- infra/live/global/core/terragrunt.hcl
+++
@@ -1,9 +0,0 @@
-include "root" {
-  path = find_in_parent_folders()
-}
-
-terraform {
-  source = "../../../modules/core"
-}
-
-inputs = {}
--- infra/modules/core/cloudtrail.tf
+++ infra/modules/core/cloudtrail.tf
@@ -10,7 +10,12 @@ resource "aws_cloudtrail" "main" {
   tags = merge(local.tags, {
     Name = "account-trail"
   })
+
+  depends_on = [
+    aws_s3_bucket_policy.cloudtrail
+  ]
 }
+
 
 # CloudTrail log bucket
 resource "aws_s3_bucket" "cloudtrail" {
@@ -47,4 +52,67 @@ resource "aws_s3_bucket_versioning" "cloudtrail" {
   versioning_configuration {
     status = "Enabled"
   }
+}
+
+data "aws_iam_policy_document" "cloudtrail_bucket" {
+  statement {
+    sid = "AWSCloudTrailAclCheck20150319"
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+
+    actions   = ["s3:GetBucketAcl"]
+    resources = [aws_s3_bucket.cloudtrail.arn]
+
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceAccount"
+      values   = [data.aws_caller_identity.current.account_id]
+    }
+  }
+
+  statement {
+    sid = "AWSCloudTrailWrite20150319"
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+
+    actions = ["s3:PutObject"]
+
+    # CloudTrail writes under this prefix
+    resources = [
+      "${aws_s3_bucket.cloudtrail.arn}/AWSLogs/${data.aws_caller_identity.current.account_id}/*"
+    ]
+
+    # CloudTrail must write with this ACL
+    condition {
+      test     = "StringEquals"
+      variable = "s3:x-amz-acl"
+      values   = ["bucket-owner-full-control"]
+    }
+
+    # Lock to your account
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceAccount"
+      values   = [data.aws_caller_identity.current.account_id]
+    }
+
+    condition {
+      test     = "ArnLike"
+      variable = "aws:SourceArn"
+      values   = [
+        "arn:aws:cloudtrail:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:trail/account-trail"
+      ]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "cloudtrail" {
+  bucket = aws_s3_bucket.cloudtrail.id
+  policy = data.aws_iam_policy_document.cloudtrail_bucket.json
 }
