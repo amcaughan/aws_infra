4f634208317f8f01c524577db9e2ea3eeea8dcb2
amcaughan <77643664+amcaughan@users.noreply.github.com>
2025-12-18 19:04:20 -0800

    severity filter on guardduty

--- infra/terragrunt/modules/guardduty-eventbridge/variables.tf
+++ infra/terragrunt/modules/guardduty-eventbridge/variables.tf
@@ -1,3 +1,9 @@
 variable "sns_topic_arn" {
   type = string
 }
+
+variable "min_severity" {
+  type        = number
+  description = "Only forward findings with severity >= this value. Set to 0 to disable filtering."
+  default     = 7
+}
